c51abcca63430121bb35380e9aa6b98829521153
James Clarke <james@clzn.dev>
2025-12-07 17:02:15 -0800

    LiveDiagnosticViewer Stub

---
+++ src/LiveDiagnosticsViewer/LiveDiagnosticsViewer/LiveDiagnosticsViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveDiagnosticsViewerApp.swift
+//  LiveDiagnosticsViewer
+//
+//  Created by James Clarke on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveDiagnosticsViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ src/LiveDiagnosticsViewer/LiveDiagnosticsViewerTests/LiveDiagnosticsViewerTests.swift
@@ -0,0 +1,16 @@
+//
+//  LiveDiagnosticsViewerTests.swift
+//  LiveDiagnosticsViewerTests
+//
+//  Created by James Clarke on 12/7/25.
+//
+
+import Testing
+
+struct LiveDiagnosticsViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
